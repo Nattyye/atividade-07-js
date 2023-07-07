26607cb474ccc9034ce22eeba605f39f17cd9faf
Natalia <132146090+Nattyye@users.noreply.github.com>
2023-07-07 18:24:31 -0300

    Add files via upload

---
+++ atv-07-natalia cristine/idex.js
@@ -0,0 +1,19 @@
+const ano_atual ="2023"
+
+//recolher o nome e ano de nascimento do usuario
+
+let nome = prompt ("digite seu nome:")
+let ano_nascimento = parseInt(prompt("Digite seu ano de nascimento:"))
+
+console.log(nome)
+console.log(ano_nascimento)
+
+let idade = ano_atual - ano_nascimento
+
+console.log("Olá " + nome + ", você tem " + idade + " anos.")
+
+
+
+
+
+    
